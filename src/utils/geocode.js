0a72a7a27eb72b60e5342f351588f64e3c6e247e
mdgreen18 <matthewdgreen18@yahoo.com>
2021-07-18 14:05:22 -0400

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWRncmVlbjE4IiwiYSI6ImNqdnY5ZmhmYzN5c2M0OG11aTg0NmU1NW4ifQ.QMrEa3KYrkHK_7gJZgPCXQ&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
